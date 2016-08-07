31bf3f932ab3227149c57c5418dcb459ff5925c5
Jethro Muller <git@jethromuller.co.za>
2016-08-07 20:12:49 +0200

    Add image saving and refactor image loading
    Make image loading more modular and register save image button.

--- src/com/jethro/MainWindow.java
+++ src/com/jethro/MainWindow.java
@@ -17,41 +17,85 @@ public class MainWindow {
     private JButton saveTabImageButton;
     private JButton detectCirclesButton;
     private JButton runEdgeDetectionButton;
+
     private JTabbedPane imageTabs;
-    private JLabel image;
-    private JLabel EdgeImage;
-    private JLabel CircleImage;
     private JPanel CircleImageTab;
     private JPanel EdgeImageTab;
     private JPanel ImageTab;
     private JPanel TabPanel;
     private JPanel MainPanel;
 
+    private JLabel imageLabel;
+    private JLabel EdgeImageLabel;
+    private JLabel CircleImageLabel;
+
     private Image baseImg;
     private Image binaryImg;
     private Image edgeImg;
     private Image circleImg;
 
+    /**
+     * Sets the JLabel icon
+     * @param label JLabel to set.
+     * @param img Image to set the JLabel's icon to.
+     */
+    private void setIconImage(JLabel label, Image img) {
+        JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, PNG, BMP & GIF Images",
+                "jpg", "png", "bmp", "gif");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(MainPanel);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                img = ImageIO.read(chooser.getSelectedFile());
+                label.setIcon(new ImageIcon(img));
+            } catch (IOException ioe) {
+                String msg = "Failed to open imageLabel: " + chooser.getSelectedFile().getName() + "\n";
+                msg += "Error: " + ioe.getMessage();
+                JOptionPane.showMessageDialog(MainPanel, msg, "Image IO Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    /**
+     * Saves the image in the currently selected tab to the directory specified by the user.
+     * @param tab JPanel to get the image from.
+     */
+    private void saveTabImage(JPanel tab) {
+        JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, PNG, BMP & GIF Images",
+                "jpg", "png", "bmp", "gif");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showSaveDialog(MainPanel);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                BufferedImage img = (BufferedImage) ((ImageIcon)((JLabel)tab.getComponent(0)).getIcon()).getImage();
+                File imgOut = chooser.getSelectedFile();
+                String[] fileParts = imgOut.getName().split("\\.");
+                String extension = fileParts[fileParts.length - 1];
+                ImageIO.write(img, extension, imgOut);
+            } catch (IOException ioe) {
+                String msg = "Failed to open imageLabel: " + chooser.getSelectedFile().getName() + "\n";
+                msg += "Error: " + ioe.getMessage();
+                JOptionPane.showMessageDialog(MainPanel, msg, "Image IO Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
     public MainWindow() {
-        // When clicked the button opens the file selector dialog and if the image is valid it sets it
+        // When clicked the button opens the file selector dialog and if the imageLabel is valid it sets it
         importImageButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, PNG, BMP & GIF Images",
-                        "jpg", "png", "bmp", "gif");
-                chooser.setFileFilter(filter);
-                int returnVal = chooser.showOpenDialog(MainPanel);
-                if(returnVal == JFileChooser.APPROVE_OPTION) {
-                    try {
-                        baseImg = ImageIO.read(chooser.getSelectedFile());
-                        image.setIcon(new ImageIcon(baseImg));
-                    } catch (IOException ioe) {
-                        String msg = "Failed to open image: " + chooser.getSelectedFile().getName() + "\n";
-                        msg += "Error: " + ioe.getMessage();
-                        JOptionPane.showMessageDialog(MainPanel, msg, "Image IO Error", JOptionPane.ERROR_MESSAGE);
-                    }
-                }
+                setIconImage(imageLabel, baseImg);
+            }
+        });
+
+        // When clicked the button opens a dialog to enable saving of the currently viewed image
+        saveTabImageButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                saveTabImage((JPanel) imageTabs.getSelectedComponent());
             }
         });
     }
@@ -61,7 +105,7 @@ public class MainWindow {
         frame.setContentPane(new MainWindow().MainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setMinimumSize(new Dimension(400, 600));
+        frame.setMinimumSize(new Dimension(800, 600));
         frame.setLocationRelativeTo(null); // Center the frame
         frame.setVisible(true);
     }
